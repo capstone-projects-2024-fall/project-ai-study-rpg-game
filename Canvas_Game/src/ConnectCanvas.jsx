ef2c32b383afc99abb602fa136e3369b25667871
tug94192 <tug94192@temple.edu>
2024-10-27 20:07:55 -0400

    Added dummy response and images

--- Canvas_Game/src/ConnectCanvas.jsx
+++ Canvas_Game/src/ConnectCanvas.jsx
@@ -16,16 +16,18 @@ export default function ConnectCanvas(){
                         To connect your account to canvas, follow these steps:
                             <ol>
                                 <li>Log in to your institution/school's canvas site</li>
-                                <li>Click "Account" in the top left</li>
-                                <li>Click "Settings"</li>
+                                <li>Click "Account" in the top left<br/><img src="src/assets/account.png"/></li>
+                                <li>Click "Settings"<br/><img src="src/assets/settings.png"/></li>
                                 <li>Scroll down to the bottom of the page</li>
-                                <li>Find the button that says "New Access Token" and click it</li>
+                                <li>Find the button that says "New Access Token" and click it<br/><img src="src/assets/accessToken.png"/></li>
                                 <li>Add Canvas Quest to the purpose, and don't enter en expiration date</li>
                                 <li>Copy the token, paste it on this page and hit connect!</li>
                             </ol>
                             <FormControl>
                                 <TextField label='Key' id='filled-basic' variant='filled'></TextField>
-                                <Button>Connect</Button>
+                                <Button id = 'connectId' onClick={function(){
+                                    alert("Canvas account registered! Click OK to continue.")
+                                }}>Connect</Button>
                             </FormControl>
                     </Typography>
                 </CardContent>
