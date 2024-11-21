2fd122b7c04ecc1b77ba97be47e105818115b146
Alyssa Richner <tui98459@temple.edu>
2024-11-20 20:09:59 -0500

    updated AssignmentsPage

--- Canvas_Game/src/AssignmentsPage.jsx
+++ Canvas_Game/src/AssignmentsPage.jsx
@@ -1,12 +1,8 @@
-//TEST TO RUN AsSIGNMENTS PAge
-
-
-
-import { useState } from "react"; 
 import React from "react";
+import { useState } from "react"; 
 
 //formatting --> more options on dashboard and userprofile
-import {
+/*import {
     Box,
     // Button,
     // IconButton,
@@ -15,33 +11,46 @@ import {
     useTheme,
   } from "@mui/material";
 import { tokens } from "./theme";
+*/
 
-
+//old imports
 import AssignmentsPageNavBar from './AssignmentsPageNavBar.jsx';
 import AssignmentsList from './AssignmentsList.jsx';
 import AssignmentsListDUMMY from './AssignmentsListDUMMYComp.jsx';
 
-import GameMapInProgressBar from './GameMapInProgressBar.jsx';
+//imported components
+import AssignmentsList2Try from './AssignmentsList2Try.jsx'; 
+import CourseDropdownMenu from './AssignmentsCourseDropdownMenu.jsx';
 
-import CourseDropdownMenu from "./AssignmentsCourseDropdownMenu.jsx";
-
+/*pick course you want to look at, assignments are displayed in a categorised list, can adjust settings*/
+    //settings:should have toggle if you dont want categories --> can do that later/ also should be able to change/add categories
 
 const AssignmentsPage = () => {
     //sets theme 
-   /* const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
+    //const theme = useTheme();
+    //const colors = tokens(theme.palette.mode);
     //adjusts based on device size
-    const isXlDevices = useMediaQuery("(min-width: 1260px)");
+    /*const isXlDevices = useMediaQuery("(min-width: 1260px)");
     const isMdDevices = useMediaQuery("(min-width: 724px)");
-    const isXsDevices = useMediaQuery("(max-width: 436px)");
-*/
+    const isXsDevices = useMediaQuery("(max-width: 436px)");*/
+
+    //function to get course selection from dropdown menu
+    const[courseValue, setCourseValue] = useState(''); 
+    
+    //send to CourseDropdownMenu to change courseValue var to whatever is selected from the dropdown menu 
+    const onCourseSelectedFromCDM = (selectedCourse) =>{ 
+        setCourseValue(selectedCourse); 
+        console.log(courseValue); //testing 
+    }
 
 
     return (
         <>
             <Box m="20px">
-
-
+                <div className="AssignmentList">
+                    <CourseDropdownMenu courseValueSelected = {onCourseSelectedFromCDM}></CourseDropdownMenu>
+                    <AssignmentsList2Try courseValueSelected = {courseValue}></AssignmentsList2Try>
+                </div>
 
                 {/*<div className = "AssignmentsListDummy">
                     <AssignmentsListDUMMY></AssignmentsListDUMMY>
@@ -62,9 +71,18 @@ const AssignmentsPage = () => {
 
                 {/*<div className="AssignmentList">
                     <AssignmentsList assignments = {assignments} course = {course} category = {category}></AssignmentsList>
-                </div>*/}
+                </div>*/}            
             </Box>
         </>
     ); 
 }
 export default AssignmentsPage
+
+
+
+//OUTLINE/NOTES
+
+//outline 
+    //Assignments Nav bar: title, settings btn, back to main menu btn 
+
+//TEST TO RUN AsSIGNMENTS PAGE
