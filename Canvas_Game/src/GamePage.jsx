4ed18b7e5285efb1a6d9f9595c6ad3fadb1fff4f
tug94192 <tug94192@temple.edu>
2024-12-05 21:49:35 -0500

    added two maps and one interior cell, interior cell is house on bottom left of map, another map is accessible by going all the way south

--- Canvas_Game/src/GamePage.jsx
+++ Canvas_Game/src/GamePage.jsx
@@ -7,7 +7,7 @@ import './index.css';
 export default function GamePage(){
     return(
         <div>
-            <iframe src='public/index.html' width='1000px' height='1000px'></iframe>
+            <iframe src='public/interior-1/index.html' width='1000px' height='1000px'></iframe>
         </div>
     );
 }
